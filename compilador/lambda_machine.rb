9e5f27bddf5617429927542fa191c8f9f7f59b24
Pedro Martinez <pvsmartinez@gmail.com>
2016-12-03 17:12:45 -0200

    lambda machine complete

--- compilador/lambda_machine.rb
+++ compilador/lambda_machine.rb
@@ -4,19 +4,137 @@ require_relative 'sintatico'
 
 class Lambda_Machine
 
-  MEM_SIZE = 512
+  MEM_SIZE = 256
 
-  attr_reader :mem #array com [512] posições.
-  attr_reader :instruction #registrador
-  attr_reader :data #registrador
-  attr_reader :result #registrador
+  NOP = 0
+  READ = 1
+  LOAD = 2
+  ADD = 3
+  PRINT = 4
+
+  INSTRUCTION = 32
+  DATA = 34
+  RESULT = 36
+
+  VALUE = 255
 
   def createMem
-    mem[0..MEM_SIZE] = 0
+    @mem[0..MEM_SIZE] = 0
+  end
+
+  def addToken(value)
+    @mem[MEM_SIZE] = value
+    MEM_SIZE ++
+  end
+
+  def execute
+    resetPointers
+
+    puts 'read ' + (MEM_SIZE - 256) + ' tokens'
+
+    while get(INSTRUCTION) > 0 && get(INSTRUCTION) < MEM_SIZE
+      printString = @mem[INSTRUCTION].to_s + ':'
+      readInstruction
+      puts printString
+    end
+
+    if get(INSTRUCTION) >= MEM_SIZE
+      puts 'INFINITY; ready.'
+    else
+      puts 'ready.'
+  end
+
+  private
+
+  def get(address)
+    return @mem[address]
   end
 
   def put(address, value)
-    mem[address] = value
+    @mem[address] = value
+  end
+
+  def readInstruction
+    opcode = get(@mem[INSTRUCTION])
+
+    if get(@mem[INSTRUCTION] + 1) == VALUE
+      # its a value
+      @mem[RESULT] = opcode
+      printString += ' ' + @mem[RESULT].to_s
+      nextInstruction
+      nextInstruction
+      return
+    end
+
+    case opcode
+    when NOP
+      printString += ' NOP'
+      nextInstruction
+    when READ
+      printString += ' (read'
+      @mem[RESULT] = readParam
+      printString += ')'
+    when LOAD
+      printString += ' load ('
+      address = readParam
+      printString += ') ('
+      value = readParam
+      printString += ')'
+      puts '[' + address.to_s + ']<--' + value.to_s
+      put address value
+    when ADD
+      printString += ' add ('
+      address = readParam
+      printString += ') ('
+      value = readParam
+      printString += ')'
+      puts '[' + address.to_s + ']+=' + value.to_s
+      sum = get(address) + value
+      put address sum
+      @mem[RESULT] = get(address)
+    when PRINT
+      printString += ' print'
+      value = readParam
+      puts '-->' + value
+    else
+      if get(opcode + 1) == VALUE
+        #variable
+        printString += ' read ' + opcode
+        @mem[RESULT] = get(opcode)
+        nextInstruction
+      else
+        #jump
+        printString += ' jump ' + opcode
+        @mem[DATA] = @mem[INSTRUCTION] + 1
+        @mem[INSTRUCTION] = opcode
+        printString += ' ('
+        readInstruction
+        printString += ')'
+      end
+    end
+  end
+
+  def readParam
+    nextInstruction
+    readInstruction
+    get @mem[RESULT]
+  end
+
+  def nextInstruction
+    @mem[INSTRUCTION] ++;
+  end
+
+  def resetPointers
+    @mem[INSTRUCTION] = 256;
+    @mem[INSTRUCTION+1] = VALUE;
+    @mem[DATA+1] = VALUE;
+    @mem[RESULT+1] = VALUE;
+
+    @mem[RESULT+2] = LOAD;
+    @mem[RESULT+3] = INSTRUCTION;
+    @mem[RESULT+4] = VALUE;
+    @mem[RESULT+5] = 0;
+    @mem[RESULT+6] = VALUE; #exit
   end
 
 end
