70aac17abd539821e2ad80e249dbdda00f55bc7e
Pedro Martinez <pvsmartinez@gmail.com>
2016-12-03 15:42:24 -0200

    lambda machine

--- compilador/lambda_machine.rb
+++ compilador/lambda_machine.rb
@@ -4,4 +4,19 @@ require_relative 'sintatico'
 
 class Lambda_Machine
 
+  MEM_SIZE = 512
+
+  attr_reader :mem #array com [512] posições.
+  attr_reader :instruction #registrador
+  attr_reader :data #registrador
+  attr_reader :result #registrador
+
+  def createMem
+    mem[0..MEM_SIZE] = 0
+  end
+
+  def put(address, value)
+    mem[address] = value
+  end
+
 end
